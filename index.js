a0b6af73ef08fb1dc31aa9d3da3f368d421e5382
blancahidalgo <b.hidalgo.ley@gmail.com>
2023-04-22 16:22:11 +1000

    fixed a couple of buggies here and there

--- index.js
+++ index.js
@@ -68,70 +68,68 @@ async function init() {
   var svgString = "";
   var svg_file = "logo.svg";
 }
-// Prompt the user for answers
-const answers = inquirer.prompt(questions);
 
-// Function below will identify the text input from the user and identify whether it's a valid entry (1-3 characters) or not
-var user_text = "";
-if (answers.text.length > 0 && answers.text.length < 4) {
-  // 1-3 chars, valid entry
-  user_text = answers.text;
-} else {
-  // 0 or 4+ chars, invalid entry
-  console.log("Wrong input my friend, make sure you enter between 1-3 characters for your logo, no more and no less");
-  return;
-}
+const answers = inquirer.prompt(questions)
+  .then((answers) => {
+    // Function below will identify the text input from the user and identify whether it's a valid entry (1-3 characters) or not
+    var user_text = "";
+    if (answers.text.length > 0 && answers.text.length < 4) {
+      // 1-3 chars, valid entry
+      user_text = answers.text;
+    } else {
+      // 0 or 4+ chars, invalid entry
+      console.log("Wrong input my friend, make sure you enter between 1-3 characters for your logo, no more and no less");
+      return;
+    }
 
-// Starts picking uo responses from the user input in CLI --> text, font color, shape color, shape type 
-console.log("User text: [" + user_text + "]");
-//user font color
-user_font_color = answers["text-color"];
-console.log("User font color: [" + user_font_color + "]");
-//user shape color
-user_shape_color = answers.shape;
-console.log("User shape color: [" + user_shape_color + "]");
-//user shape type
-user_shape_type = answers["pixel-image"];
-console.log("User entered shape = [" + user_shape_type + "]");
-
-
-// Running a variable with if statements to identify the shape type and color selected by the user
-let user_shape;
-if (user_shape_type === "Square" || user_shape_type === "square") {
-  user_shape = new Square();
-  console.log("User selected Square shape");
-}
-else if (user_shape_type === "Circle" || user_shape_type === "circle") {
-  user_shape = new Circle();
-  console.log("User selected Circle shape");
-}
-else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
-  user_shape = new Triangle();
-  console.log("User selected Triangle shape");
-}
-else {
-  console.log("Wrong shape selection out of the options we have!");
-}
-user_shape.setColor(user_shape_color);
-
-// Create a new Svg instance and add the shape and text elements to it
-var svg = new Svg();
-svg.setTextElement(user_text, user_font_color);
-svg.setShapeElement(user_shape);
-svgString = svg.render();
-
-//Print shape to log
-console.log("Displaying shape:\n\n" + svgString);
-//document.getElementById("svg_image").innerHTML = svgString;
-
-console.log("Shape generation complete!");
-console.log("Writing shape to file...");
-writeToFile(svg_file, svgString);
-
-init()
+    // Starts picking uo responses from the user input in CLI --> text, font color, shape color, shape type 
+    console.log("User text: [" + user_text + "]");
+    //user font color
+    user_font_color = answers["text-color"];
+    console.log("User font color: [" + user_font_color + "]");
+    //user shape color
+    user_shape_color = answers.shape;
+    console.log("User shape color: [" + user_shape_color + "]");
+    //user shape type
+    user_shape_type = answers["pixel-image"];
+    console.log("User entered shape = [" + user_shape_type + "]");
 
 
+    // Running a variable with if statements to identify the shape type and color selected by the user
+    let user_shape;
+    if (user_shape_type === "Square" || user_shape_type === "square") {
+      user_shape = new Square();
+      console.log("User selected Square shape");
+    }
+    else if (user_shape_type === "Circle" || user_shape_type === "circle") {
+      user_shape = new Circle();
+      console.log("User selected Circle shape");
+    }
+    else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
+      user_shape = new Triangle();
+      console.log("User selected Triangle shape");
+    }
+    else {
+      console.log("Wrong shape selection out of the options we have!");
+    }
+    user_shape.setColor(user_shape_color);
 
+    // Create a new Svg instance and add the shape and text elements to it
+    var svg = new Svg();
+    svg.setTextElement(user_text, user_font_color);
+    svg.setShapeElement(user_shape);
+    svgString = svg.render();
+
+    //Print shape to log
+    console.log("Displaying shape:\n\n" + svgString);
+    //document.getElementById("svg_image").innerHTML = svgString;
+
+    console.log("Shape generation complete!");
+    console.log("Writing shape to file...");
+    writeToFile(svg_file, svgString);
+
+    init()
+  });
 
 
 
@@ -152,7 +150,96 @@ init()
 
 
 
-  //   // Saves the SVG file to disk
-  //   fs.writeFileSync('logo.svg', captcha.data);
-  // });
+
+
+
+
+
+// // Prompt the user for answers
+// const answers = inquirer.prompt(questions);
+
+// // Function below will identify the text input from the user and identify whether it's a valid entry (1-3 characters) or not
+// var user_text = "";
+// if (answers.text.length > 0 && answers.text.length < 4) {
+//   // 1-3 chars, valid entry
+//   user_text = answers.text;
+// } else {
+//   // 0 or 4+ chars, invalid entry
+//   console.log("Wrong input my friend, make sure you enter between 1-3 characters for your logo, no more and no less");
+//   return;
+// }
+
+// // Starts picking uo responses from the user input in CLI --> text, font color, shape color, shape type 
+// console.log("User text: [" + user_text + "]");
+// //user font color
+// user_font_color = answers["text-color"];
+// console.log("User font color: [" + user_font_color + "]");
+// //user shape color
+// user_shape_color = answers.shape;
+// console.log("User shape color: [" + user_shape_color + "]");
+// //user shape type
+// user_shape_type = answers["pixel-image"];
+// console.log("User entered shape = [" + user_shape_type + "]");
+
+
+// // Running a variable with if statements to identify the shape type and color selected by the user
+// let user_shape;
+// if (user_shape_type === "Square" || user_shape_type === "square") {
+//   user_shape = new Square();
+//   console.log("User selected Square shape");
+// }
+// else if (user_shape_type === "Circle" || user_shape_type === "circle") {
+//   user_shape = new Circle();
+//   console.log("User selected Circle shape");
+// }
+// else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
+//   user_shape = new Triangle();
+//   console.log("User selected Triangle shape");
+// }
+// else {
+//   console.log("Wrong shape selection out of the options we have!");
+// }
+// user_shape.setColor(user_shape_color);
+
+// // Create a new Svg instance and add the shape and text elements to it
+// var svg = new Svg();
+// svg.setTextElement(user_text, user_font_color);
+// svg.setShapeElement(user_shape);
+// svgString = svg.render();
+
+// //Print shape to log
+// console.log("Displaying shape:\n\n" + svgString);
+// //document.getElementById("svg_image").innerHTML = svgString;
+
+// console.log("Shape generation complete!");
+// console.log("Writing shape to file...");
+// writeToFile(svg_file, svgString);
+
+// init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   //   // Saves the SVG file to disk
+//   //   fs.writeFileSync('logo.svg', captcha.data);
+//   // });
 
